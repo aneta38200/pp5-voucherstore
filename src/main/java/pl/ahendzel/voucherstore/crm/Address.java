3de3b62254d2862674d686aaa4cd11f3cc74f948
aneta38200 <aneta38200@onet.eu>
2020-12-05 13:36:20 +0100

    introduce crm simple with the power of jpa

---
+++ src/main/java/pl/ahendzel/voucherstore/crm/Address.java
@@ -0,0 +1,16 @@
+package pl.ahendzel.voucherstore.crm;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+public class Address {
+    private String street;
+    private String zip;
+    private String city;
+
+}
